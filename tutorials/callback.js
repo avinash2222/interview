a298ed2517696cfd6c29bf5f4aa880b61d928b14
avinash_personal <avinashcat.singh2222@gmail.com>
2021-06-17 19:29:21 +0530

    added callback file

--- tutorials/callback.js
+++ tutorials/callback.js
@@ -1,21 +1,53 @@
 
 
 console.log('in tutorials callback file..')
-//-----------------------------------------------
+//--------------------1---------------------------
 
 
-function doHomework(subject, callback) {
-  console.log(`Starting my ${subject} homework.`)
-  callback()
+// function doHomework(subject, callback) {
+//   console.log(`Starting my ${subject} homework.`)
+//   callback()
+// }
+
+// function testCall(data, callback) {
+//   console.log('in test callback')
+//   callback()
+// }
+// doHomework('math', function() {
+//   console.log('Finished my homework')
+//   testCall('rinku', function() {
+//     console.log('test callback finishes...')
+//   })
+// })
+
+//--------------2------------------------------------------
+
+const cbTest = () => {
+  setTimeout(()=>{
+    return {
+      name:'DOG',
+      sex: 'MALE',
+    }
+  }, 1000)
 }
 
-function testCall(data, callback) {
-  console.log('in test callback')
-  callback()
+let result = cbTest()
+console.log('result: ',result)
+//---------------------------
+const cbTest2 = (cb) => {
+  setTimeout(()=>{
+    let data =  {
+      name:'DOG',
+      sex: 'MALE',
+    }
+    cb(data)
+  }, 1000)
 }
-doHomework('math', function() {
-  console.log('Finished my homework')
-  testCall('rinku', function() {
-    console.log('test callback finishes...')
-  })
-})
+
+cbTest2((data)=> console.log('result2:', data))
+
+// const cb = (data) => {
+//   console.log('result2:', data)
+// }
+// cbTest2(cb)
+//------------------------------------------
