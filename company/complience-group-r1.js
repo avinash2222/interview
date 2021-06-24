e91c613e0879ddc1924386df40877e6b4244a0d6
avinash_personal <avinashcat.singh2222@gmail.com>
2021-06-24 20:03:57 +0530

    complience group intervie

---
+++ company/complience-group-r1.js
@@ -0,0 +1,23 @@
+// function counter() {
+//   var n=0
+//   return {
+//     count: function() {return n++},
+//     reset: function() {n=0},
+//   }
+// }
+
+// var c = counter()
+// var d = counter()
+// c.count()
+// c.count()
+// c.reset()
+// d.count()
+// d.reset()
+
+//--------------------------------------------------
+// Following is the syntax of the method to open a file in asynchronous mode −
+
+// fs.open(path, flags[, mode], callback)
+//-------------------------------------------------
+// coding question
+// Check if a string can be formed from another string by at most X circular clockwise shifts
