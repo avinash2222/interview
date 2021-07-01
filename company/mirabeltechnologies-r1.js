4be2bec2539e720268556d2cbb4ae404f9052730
avinash_personal <avinashcat.singh2222@gmail.com>
2021-07-01 19:29:54 +0530

    mirabeltechnologies

---
+++ company/mirabeltechnologies-r1.js
@@ -0,0 +1,7 @@
+/*
+1. how to manage state in react
+2. can u write one function component
+3. what are all es6 features
+4. hoisting
+5. const let var
+*/
---
+++ tutorials/currying.js
@@ -0,0 +1,6 @@
+/*
+Currying is a transformation of functions that translates a function from callable as f(a, b, c) into callable as f(a)(b)(c).
+Currying doesn’t call a function. It just transforms it.
+ In other words, curry(f) for two-argument f(a, b) translates it into a function that runs as f(a)(b)
+
+*/
