3358ca4448c4e5621d65f36eeef9072faeccf422
avinash_personal <avinashcat.singh2222@gmail.com>
2021-07-06 18:33:59 +0530

    creditSafe

---
+++ company/creditSafe-r1.js
@@ -0,0 +1,14 @@
+/*
+1. type of load balance
+2. any encryption while storing data in s3
+3. what is ec2
+4. are u using any cloud Watch or something
+5. mem cache
+6. redis
+7. auto scale
+8. s3 classes
+
+
+
+
+*/
