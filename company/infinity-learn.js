8b637e77d57b4e6de076f0aa2d6ae33eed84337b
avinash_personal <avinashcat.singh2222@gmail.com>
2021-07-06 15:29:36 +0530

    infinity learn

---
+++ company/infinity-learn.js
@@ -0,0 +1,17 @@
+/*
+
+> authorization
+> file upload
+> socket connection initiation 
+> load balancing (handling multiple request)
+> horizontal scaling
+> vertical scaling
+> which is good horizontal/vertical
+> timestamp / UUID (internal structure)
+> mem queue 
+> redis 
+> profiling 
+> compound indexing (why we use it)
+> node request is cpu consuming or memory consuming
+
+*/
