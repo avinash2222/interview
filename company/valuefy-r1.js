6c48d1af96b2bab9619e12ef17cd76db211221ea
avinash_personal <avinashcat.singh2222@gmail.com>
2021-07-10 13:12:37 +0530

    valuefy

---
+++ company/valuefy-r1.js
@@ -0,0 +1,11 @@
+/*
+  1. which object creation dont allow delete feature
+  2. how to check if object if empty or not
+  3. how we can implement inheritance in js
+  4. null == undefined
+  5. null === undefined
+  6. working of any website when we open it
+  7. promises
+  8. 
+
+*/
