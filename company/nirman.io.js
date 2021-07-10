0dce7b0e575f83a2b4c0194e1e611073563ba227
avinash_personal <avinashcat.singh2222@gmail.com>
2021-07-10 15:18:54 +0530

    nirman.io

---
+++ company/nirman.io.js
@@ -0,0 +1,16 @@
+/*
+  1. sharding 
+  2. replica
+  3. REPL
+  4. what is process in process.env
+  5. what is event and event emitter 
+  6. what is setImmediate 
+  7. stream in nodejs
+  8. pipe/chain in nodejs
+  9. why node js
+
+
+
+
+
+*/
