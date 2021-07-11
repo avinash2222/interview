2ad7e7308574eb782c29192db4d446671ae70ad3
avinash_personal <avinashcat.singh2222@gmail.com>
2021-07-11 13:37:01 +0530

    bimaplan

---
+++ company/bimaPlan-r1.js
@@ -0,0 +1,9 @@
+/*
+  1. in database 1 m record added each day latency of fetch api become 1 ms to 10 ms how u handle this
+  2. oops concept
+  3. how u upload large size file in s3 like 10 gb
+  4. how u creating jwt token
+  5. any other framework apart from mvc
+  6. what is nonblocking 
+
+*/
